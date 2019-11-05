44931d65ac812da33bbad1dc032c600296e702c5
Apostolos <apostolos@projectbeagle.com>
2019-11-05 12:40:51 +0200

    Moved stereopanner into different file

--- js/noise-module.js
+++ js/noise-module.js
@@ -1093,46 +1093,6 @@
 
 
 	/**
-	 * StereoPannerModuleNode: Class for 'stereopannernode' node
-	 */
-
-	$.StereoPannerModuleNode		= function ( noiseModule ) {
-
-		this.nm = noiseModule;
-
-	};
-
-	$.StereoPannerModuleNode.prototype	= {
-
-		createModuleAudioNode	: function ( module ) {
-
-			var node = this.nm.audioContext.createStereoPanner ( );
-
-			node.pan.value = module.options.stereoPannerPan;
-
-			return node;
-
-		},
-
-		createModuleDiv		: function ( $moduleEl, module, audioNode ) {
-
-			var $panDiv = this.nm._createSimpleSliderControl( audioNode, 'pan', -1, 1, 0.01, "" );
-
-			$panDiv.appendTo( $moduleEl );
-
-		},
-
-		resetModuleSettings	: function ( $moduleEl, module, audioNode ) {
-
-			this.nm._resetSliderSetting( $moduleEl, audioNode, 'pan', module.options.stereoPannerPan );
-
-		}
-
-	};
-
-
-
-	/**
 	 * WaveShaperModuleNode: Class for 'waveshapernode' node
 	 */
 
---
+++ js/stereopanner-module-node.js
@@ -0,0 +1,40 @@
+( function( window, navigator, $, undefined ) {
+
+    /**
+	 * StereoPannerModuleNode: Class for 'stereopannernode' node
+	 */
+	$.StereoPannerModuleNode           = function ( noiseModule ) {
+
+		this.nm = noiseModule;
+
+	};
+
+	$.StereoPannerModuleNode.prototype = {
+
+		createModuleAudioNode : function ( module ) {
+
+			var node = this.nm.audioContext.createStereoPanner ( );
+
+			node.pan.value = module.options.stereoPannerPan;
+
+			return node;
+
+		},
+
+		createModuleDiv       : function ( $moduleEl, module, audioNode ) {
+
+			var $panDiv = this.nm._createSimpleSliderControl( audioNode, 'pan', -1, 1, 0.01, "" );
+
+			$panDiv.appendTo( $moduleEl );
+
+		},
+
+		resetModuleSettings   : function ( $moduleEl, module, audioNode ) {
+
+			this.nm._resetSliderSetting( $moduleEl, audioNode, 'pan', module.options.stereoPannerPan );
+
+		}
+
+	};
+
+} )( window, navigator, jQuery );
