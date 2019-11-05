db147a8286302a987ab4ae6e812675117abb8e01
Apostolos <apostolos@projectbeagle.com>
2019-11-05 12:36:18 +0200

    Moved Gain into different file

--- js/noise-module.js
+++ js/noise-module.js
@@ -1093,42 +1093,6 @@
 
 
 	/**
-	 * GainModuleNode: Class for 'gain' node
-	 */
-
-	$.GainModuleNode		= function ( noiseModule ) {
-
-		this.nm = noiseModule;
-
-	};
-
-	$.GainModuleNode.prototype	= {
-
-		createModuleAudioNode	: function ( module ) {
-
-			return this.nm._createGain( module );
-
-		},
-
-		createModuleDiv		: function ( $moduleEl, module, audioNode ) {
-
-			var $gainDiv	= this.nm._createSimpleSliderControl( audioNode, 'gain', 0, 1, 0.01, "" );
-
-			$gainDiv.appendTo( $moduleEl );
-
-		},
-
-		resetModuleSettings	: function ( $moduleEl, module, audioNode ) {
-
-			this.nm._resetSliderSetting( $moduleEl, audioNode, 'gain', module.options.gainGain );
-
-		},
-
-	};
-
-
-
-	/**
 	 * StereoPannerModuleNode: Class for 'stereopannernode' node
 	 */
 
