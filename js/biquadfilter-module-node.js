0015ea2867c01ca8521970816197bc961b38ef8a
Apostolos <apostolos@projectbeagle.com>
2019-11-05 12:02:30 +0200

    Moved SoundCloud and BiquadFilter into different files

---
+++ js/biquadfilter-module-node.js
@@ -0,0 +1,44 @@
+( function( window, navigator, $, undefined ) {
+
+    /**
+	 * BiquadFilterModuleNode: Class for 'biquadfilter' node
+	 */
+	$.BiquadFilterModuleNode           = function ( noiseModule ) {
+
+		this.nm = noiseModule;
+	};
+
+	$.BiquadFilterModuleNode.prototype = {
+
+		createModuleAudioNode : function ( module ) {
+
+			return this.nm._createBiquadFilter( module );
+
+		},
+
+		createModuleDiv       : function ( $moduleEl, module, audioNode ) {
+
+			var $freqDiv	= this.nm._createSimpleSliderControl( audioNode, 'frequency', 0, 8000, 1, "Hz" );
+			var $detuDiv	= this.nm._createSimpleSliderControl( audioNode, 'detune', -1200, 1200, 1, "cents" );
+			var $qDiv	= this.nm._createSimpleSliderControl( audioNode, 'Q', 1, 100, 0.1, "" );
+			var $gainDiv	= this.nm._createSimpleSliderControl( audioNode, 'gain', 0, 1, 0.01, "" );
+
+			$freqDiv.appendTo( $moduleEl );
+			$detuDiv.appendTo( $moduleEl );
+			$qDiv.appendTo( $moduleEl );
+			$gainDiv.appendTo( $moduleEl );
+
+		},
+
+		resetModuleSettings   : function ( $moduleEl, module, audioNode ) {
+
+			this.nm._resetSliderSetting( $moduleEl, audioNode, 'frequency', module.options.biquadFilterFrequency );
+			this.nm._resetSliderSetting( $moduleEl, audioNode, 'detune', module.options.biquadFilterDetune );
+			this.nm._resetSliderSetting( $moduleEl, audioNode, 'Q', module.options.biquadFilterQ );
+			this.nm._resetSliderSetting( $moduleEl, audioNode, 'gain', module.options.biquadFilterGain );
+
+		},
+
+	};
+
+} )( window, navigator, jQuery );
---
+++ js/soundcloud-module-node.js
@@ -0,0 +1,116 @@
+( function( window, navigator, $, undefined ) {
+
+    /**
+	 * SoundCloudModuleNode: Class for 'soundcloudnode' node
+	 */
+	$.SoundCloudModuleNode             = function ( noiseModule ) {
+
+		this.nm = noiseModule;
+
+	};
+
+	$.SoundCloudModuleNode.prototype   = {
+
+		createModuleAudioNode     : function ( module ) {
+
+			var _self 		= this;
+
+			var source;
+
+			var baseUrl		= 'https://api.soundcloud.com/resolve.json?url=';
+			var clientParameter 	= 'client_id=' + module.options.soundCloudClientId;
+			var url			= baseUrl + module.options.soundCloudTrackUrl + '&' + clientParameter;
+
+			var audio 		= new Audio( );
+			audio.crossOrigin	= "anonymous";
+
+			module.options.soundCloudAudio = audio;
+
+			this.nm._requestGET( url, function ( response ) {
+
+				var trackInfo 	= JSON.parse( response );
+				var streamUrl 	= trackInfo.stream_url + "?" + clientParameter;
+
+				audio.src 	= streamUrl;
+
+				source 		= _self.nm.audioContext.createMediaElementSource( audio );
+
+				/* Update source node map with this new instance */
+				_self.nm._updateAudioNode( module.name, source );
+
+				var $divEl 	= _self.nm._findModuleDivByName( module );
+				var $content 	= $( $divEl ).find( '.nm-content' );
+
+				_self.nm._appendModuleFooter( $( $divEl ), $content, module, source );
+				_self.nm._connectAllDestinations( module );
+
+				/* If module option is started then do the connection */
+				if (module.options.started) {
+					audio.play( );
+				}
+			} );
+
+		},
+
+		createModuleDiv           : function ( $moduleEl, module, audioNode ) {
+
+			var template 	= '<span class="nm-label"></span>';
+			var $span 	= $( template );
+
+			var audio 	= module.options.soundCloudAudio;
+
+			if (!audio) {
+
+				$span.text( 'Could not connect...' );
+				$span.appendTo( $moduleEl );
+
+				return $span;
+			};
+
+			var $audioEl 	= $( audio );
+
+			$audioEl.on( 'playing', function( e ) { $span.text( 'Playing' ); } );
+			$audioEl.on( 'pause', function( e ) { $span.text( 'Paused' ); } );
+			$audioEl.on( 'play', function( e ) { $span.text( 'Play' ); } );
+			$audioEl.on( 'ended', function( e ) { $span.text( 'Ended' ); } );
+			$audioEl.on( 'seeking', function( e ) { $span.text( 'Seeking' ); } );
+			$audioEl.on( 'waiting', function( e ) { $span.text( 'Waiting' ); } );
+
+
+			$span.appendTo( $moduleEl );
+
+			this.nm._createPlayPauseButton( $moduleEl, module, audioNode, this._soundCloudPlayPauseEvent );
+
+		},
+
+		resetModuleSettings       : function ( $moduleEl, module, audioNode ) {
+
+			var audio	= module.options.soundCloudAudio;
+			var $img	= $moduleEl.find( '.nm-play-button' );
+
+			audio.pause( );
+
+			$img.removeClass( 'pause' );
+			$img.addClass( 'play' );
+
+			audio.load( );
+
+		},
+
+
+		/* Private Methods */
+		_soundCloudPlayPauseEvent : function ( self, $moduleEl, module, audioNode, playPause ) {
+
+			var audio 		= module.options.soundCloudAudio;
+
+			if (audio.paused) {
+				audio.play( );
+			}
+			else {
+				audio.pause( );
+			};
+
+		},
+	};
+
+} )( window, navigator, jQuery );
